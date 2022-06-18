d4acf1800f9329aa0e0fdf981405396e28259189
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-06-17 21:51:07 -0300

    estrutura HTML, css e logica js quase pronta

--- calculadora.js
+++ calculadora.js
@@ -1,72 +1,85 @@
-function calculadora() {
-    const operacao = Number(prompt('Escolha uma opção \n 1: Somar \n 2: Subtrair \n 3: Mutilplicação \n 4: Divisão'));
+// function calculadora() {
+//     const operacao = Number(prompt('Escolha uma opção \n 1: Somar \n 2: Subtrair \n 3: Mutilplicação \n 4: Divisão'));
 
-    if (operacao < 5 && operacao > 0) {
-        let n1 = Number(prompt('Digite um número'));
-        let n2 = Number(prompt('Digite um número'));
+//     if (operacao < 5 && operacao > 0) {
+//         let n1 = Number(prompt('Digite um número'));
+//         let n2 = Number(prompt('Digite um número'));
 
-        if (!n1 || !n2) {
-            alert("Dados invalidos");
-            calculadora();
-        }
+//         if (!n1 || !n2) {
+//             alert("Dados invalidos");
+//             calculadora();
+//         }
 
-        function soma() {
-            let resul = n1 + n2;
-            alert(`Resultado é ${resul}`);
-            novaoperacao();
-        }
-        function subtrair() {
-            let resul = n1 - n2;
-            alert(`Resultado é ${resul}`);
-            novaoperacao();
-        }
-        function mutilplicação() {
-            let resul = n1 * n2;
-            alert(`Resultado é ${resul}`);
-            novaoperacao();
-        }
-        function divisao() {
-            let resul = n1 / n2;
-            alert(`Resultado é ${resul}`);
-            novaoperacao();
-        }
+//         function soma() {
+//             let resul = n1 + n2;
+//             alert(`Resultado é ${resul}`);
+//             novaoperacao();
+//         }
+//         function subtrair() {
+//             let resul = n1 - n2;
+//             alert(`Resultado é ${resul}`);
+//             novaoperacao();
+//         }
+//         function mutilplicação() {
+//             let resul = n1 * n2;
+//             alert(`Resultado é ${resul}`);
+//             novaoperacao();
+//         }
+//         function divisao() {
+//             let resul = n1 / n2;
+//             alert(`Resultado é ${resul}`);
+//             novaoperacao();
+//         }
 
-        switch (operacao) {
-            case 1:
-                soma();
-                break;
-            case 2:
-                subtrair();
-                break;
-            case 3:
-                mutilplicação();
-                break;
-            case 4:
-                divisao();
-                break;
+//         switch (operacao) {
+//             case 1:
+//                 soma();
+//                 break;
+//             case 2:
+//                 subtrair();
+//                 break;
+//             case 3:
+//                 mutilplicação();
+//                 break;
+//             case 4:
+//                 divisao();
+//                 break;
 
-        }
-    } else {
-        alert("Número invalido !!");
-        calculadora();
-    }
+//         }
+//     } else {
+//         alert("Número invalido !!");
+//         calculadora();
+//     }
 
-    function novaoperacao(){
-        const opcao = Number(prompt("Deseja fazer outra operação? \n1: Sim \n2: Não"));
+//     function novaoperacao(){
+//         const opcao = Number(prompt("Deseja fazer outra operação? \n1: Sim \n2: Não"));
     
-        switch (opcao){
-            case 1:
-                calculadora();
-                break;
-            case 2:
-                alert("Até mais");
-                break;
-            default:
-                alert("Dados invalidos");
-                novaoperacao();
-                break;
-        }
-    }
+//         switch (opcao){
+//             case 1:
+//                 calculadora();
+//                 break;
+//             case 2:
+//                 alert("Até mais");
+//                 break;
+//             default:
+//                 alert("Dados invalidos");
+//                 novaoperacao();
+//                 break;
+//         }
+//     }
 
+// }
+// calculadora();
+
+function insert(num){
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
 }
-calculadora();
+
+function clean(){
+    document.getElementById('resultado').innerHTML = ("");
+}
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
